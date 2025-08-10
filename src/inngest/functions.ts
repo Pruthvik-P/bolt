f61e342efab2b0ba1d5d0c8171644275befc1d88
Pruthvik <pruthvik@lokveeraj.com>
2025-08-10 15:45:00 +0530

    05:AI-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
+import { gemini, createAgent} from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "summarizer",
+      system: "you are a expert nextjs developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({model:"gemini-2.0-flash"}),
+    });
+    const {output} = await codeAgent.run(
+      `Write the following text: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
